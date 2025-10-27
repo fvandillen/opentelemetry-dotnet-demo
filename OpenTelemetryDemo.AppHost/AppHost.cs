af5476dbd65a372b0896cb16f15051e7cd14caa7
Florian van Dillen <f.vandillen@4dotnet.nl>
2025-10-27 14:43:28 +0100

    Run demo projects with Aspire, use ServiceDefaults.

--- OpenTelemetryDemo.AppHost/AppHost.cs
+++ OpenTelemetryDemo.AppHost/AppHost.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var inventoryApi = builder.AddProject<Projects.OpenTelemetryDemo_InventoryApi>("inventory-api");
+
+builder.AddProject<Projects.OpenTelemetryDemo_OrderingApi>("ordering-api").WithReference(inventoryApi);
+
 builder.Build().Run();
